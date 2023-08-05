98ee62c565394fb5c759d4067d09afb189cace29
Neja Atapattu <nejaatapattu@gmail.com>
2023-08-05 21:09:49 +0300

    add more requests

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('HomePage')
+})
+
+app.get('/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/signup', (req, res) => {
+  res.send('signup!')
 })
 
 app.listen(port, () => {
